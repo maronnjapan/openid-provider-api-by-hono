8909c2cde9a63cc02b51753db8fb37b73365cd3b
maronnjapan <tihoutaikai20@gmail.com>
2025-02-19 14:06:09 +0900

    fix(domain)：プロパティ名の変更

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -2,6 +2,6 @@ export class User {
     constructor(
         public readonly id: string,
         public readonly name: string,
-        public readonly email: string,
+        public readonly password: string,
     ) { }
 }
